5ab53b1b75a97eed01aab38c2bdd912e95156f56
Dell <mradulkatiyar1996@gmail.com>
2023-11-15 11:12:43 +0530

    first commit

---
+++ src/myjavagitdemo/firstDemo.java
@@ -0,0 +1,11 @@
+package myjavagitdemo;
+
+public class firstDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java world !!");
+
+	}
+
+}
